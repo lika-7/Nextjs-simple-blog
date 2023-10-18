68206073a34f5bdcafc7bb92530498484900ea53
lika-7 <dhwhdtn93@gmail.com>
2023-10-18 17:03:51 +0900

    15. 마크다운 데이터 경로에 따라 보여주는 기능 추가

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -1,8 +1,47 @@
+import { GetStaticPaths, GetStaticProps } from 'next'
 import React from 'react'
+import { getAllPostIds, getPostData } from '../../lib/post'
+import Head from 'next/head'
+import homeStyles from '../styles/Home.module.css'
 
-const Post = () =>{
+const Post = ({postData}:{
+    postData:{
+        title: string,
+        date: string,
+        contentHtml: string
+    }
+}) =>{
     return (
-        <div>[id]</div>
+        <div>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <article>
+                <h1 >{postData.title}</h1>
+                <div>
+                    {postData.date}
+                </div>
+                <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
+            </article>
+        </div>
     )
 }
 export default Post
+
+export const getStaticPaths: GetStaticPaths = function async (){
+    const paths = getAllPostIds()
+    //[{params: {id:`ore-rendering`},{params...}}]
+    return{
+        paths,
+        fallback: false
+    }
+}
+
+export const getStaticProps: GetStaticProps = async function async({params}){
+    const postData = await getPostData(params?.id as string)
+    return{
+        props: {
+            postData
+        }
+    }
+}
